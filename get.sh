c332a1f72a02133bd8f0b1af71bf047a30c80dda
eric <eric@frotmail.nl>
2026-05-28 09:00:57 +0200

    Poging 3 om de initiele zaken te fixen

--- get.sh
+++ get.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-## Welkom bij de Docker workshop.
-# Dit script installeert een aantal requirements 
-# en zet vervolgens een kopie van de repo klaar.
-
+# =============================================================================
+# Welkom bij de Docker workshop.
 # Usage: wget -O - https://docker.frotmail.nl/ | bash
+#
+# Dit script zet wat basis zaken klaar:
+# 1. Het instaleert de packages ansible en git, deze hebben we later nodig
+# 2. Het maakt een map /opt/workshop en maakt de normale user eigenaar
+# 3. Het maakt een kopie van de repository op github in de map /opt/workshop
+# =============================================================================
+
+# Configuration:
+REPO_URL="https://github.com/DITCOnsultants/DockerWorkshop.git"
+TARGET_DIR="/opt/workshop"
+TARGET_USER_ID=1000
 
 # Controleer eerst of we de juiste rechten hebben om zaken te installeren:
 if [[ $EUID -ne 0 ]]
@@ -12,25 +21,28 @@ then
   echo "Please run this script as root!"
   exit 1
 fi
+# Installeer packages
 echo "Installing required packages..."
 apt install -y \
   git \
   ansible 1> /dev/null
 
-# Geef de normale user docker rechten en clone de repo
+# Geef de normale user docker rechten
 echo "Give regular user docker permissions"
-TARGET_USER=$(id -nu 1000 2>/dev/null)
+TARGET_USER=$(id -nu $TARGET_USER_ID 2>/dev/null)
 if [ -z "$TARGET_USER" ]; then
-    echo "Error: No user found with UID 1000."
+    echo "Error: No user found with UID $TARGET_USER_ID."
     exit 1
 fi
 usermod -a -G docker "$TARGET_USER"
-mkdir /opt/workshop
-chown 1000:1000 /opt/workshop
 
+# Maak de workshop map en geef user rechten
+mkdir $TARGET_DIR
+chown $TARGET_USER_ID:$TARGET_USER_ID $TARGET_DIR
+
+# Clone repo
 echo "Switching to user $TARGET_USER"
 su - "$TARGET_USER" << EOF
-  echo "Cloning repo into /opt/workshop" 
-  cd /opt/
-  git clone https://github.com/DITCOnsultants/DockerWorkshop.git workshop
+  echo "Cloning repo into $TARGET_DIR" 
+  git clone https://github.com/DITCOnsultants/DockerWorkshop.git $TARGET_DIR
 EOF
