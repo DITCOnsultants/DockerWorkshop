d24f6e76187ce4df3fb38eeb71cd8a52dedd2c75
eric <eric@frotmail.nl>
2026-05-28 08:46:30 +0200

    Poging 1 om de initiele zaken te fixen

--- get.sh
+++ get.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
-apt install -y git ansible
-git clone https://github.com/DITCOnsultants/DockerWorkshop.git
+
+## Welkom bij de Docker workshop.
+# Dit script installeert een aantal requirements 
+# en zet vervolgens een kopie van de repo klaar.
+
+# Usage: wget -O - https://docker.frotmail.nl/ | sh
+
+# Controleer eerst of we de juiste rechten hebben om zaken te installeren:
+if [[ $EUID -ne 0 ]]
+then
+  echo "Please run this script as root!"
+  exit 1
+fi
+echo "Installing required packages..."
+apt install -y \
+  git \
+  ansible 1> /dev/null
+
+# Geef de normale user docker rechten en clone de repo
+echo "Give regular user docker permissions"
+TARGET_USER=$(id -nu 1000 2>/dev/null)
+if [ -z "$TARGET_USER" ]; then
+    echo "Error: No user found with UID 1000."
+    exit 1
+fi
+usermod -a -G docker "$TARGET_USER"
+
+echo "Switching to user $TARGET_USER"
+su - "$TARGET_USER" << EOF
+  echo "Cloning repo into /opt/workshop" 
+  mkdir /opt/workshop
+  cd /opt/
+  git clone https://github.com/DITCOnsultants/DockerWorkshop.git workshop
+EOF
